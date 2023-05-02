d791d126027cc1736ad889fc03f92b8c688ca3a4
Jummy89 <104064323+Jummy89@users.noreply.github.com>
2023-05-02 22:27:56 +0100

    Add files via upload

---
+++ employeepayroll.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import Combobox
+import random
+import time
+import datetime
+from tkcalendar import *
+from tkcalendar import *
+from tkinter import messagebox
+import mysql.connector
+
